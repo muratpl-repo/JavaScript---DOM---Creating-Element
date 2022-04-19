b10ef6f0f4d69a16351666e325f130fac229eccd
muratpala007 <muratpala007@gmail.com>
2022-04-19 22:42:39 +0300

    first commit

---
+++ myscripts.js
@@ -0,0 +1,5 @@
+var paragraf = document.createElement("p");
+    document.body.appendChild(paragraf);
+
+    var paragrafMetni = document.createTextNode("Ben bir paragraf icerigiyim.");
+    paragraf.appendChild(paragrafMetni);
